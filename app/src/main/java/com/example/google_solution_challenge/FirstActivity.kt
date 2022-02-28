ea4ab4231de3480ce5266a14a21511d812009745
Yao Zhang <alanzhangyao@outlook.com>
2022-02-28 16:30:05 -0500

    switch from activity to fragment

--- app/src/main/java/com/example/google_solution_challenge/FirstActivity.kt
+++ app/src/main/java/com/example/google_solution_challenge/FirstActivity.kt
@@ -15,40 +15,10 @@ import com.example.google_solution_challenge.databinding.ActivityFirstBinding
 import com.google.android.material.textfield.TextInputLayout
 
 class FirstActivity : AppCompatActivity() {
-    lateinit var autoCompleteTextView: AutoCompleteTextView
-    lateinit var continueButton : Button
-    var selected = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
-        val items = resources.getStringArray(R.array.universities)
-        val adapter = ArrayAdapter(this, R.layout.list_item, items)
-        autoCompleteTextView = findViewById(R.id.options)
-        autoCompleteTextView.setAdapter(adapter)
-        continueButton = findViewById(R.id.continueButton)
-        continueButton.setVisibility(View.INVISIBLE)
-        autoCompleteTextView.addTextChangedListener(object: TextWatcher {
-            override fun afterTextChanged(s: Editable?) {
-                selected = s.toString()
-                continueButton.setVisibility(View.VISIBLE)
-            }
-
-            override fun beforeTextChanged(
-                s: CharSequence?,
-                start: Int,
-                count: Int,
-                after: Int
-            ) {
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-
-            }
-        })
-        fun onContinue(view: View)
-        {
-            //proceed to next screen with the selected university.
-        }
         
     }
     
---
+++ app/src/main/java/com/example/google_solution_challenge/UniversityFragment.kt
@@ -0,0 +1,66 @@
+package com.example.google_solution_challenge
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.example.google_solution_challenge.databinding.FragmentUniversityBinding
+
+
+class UniversityFragment : Fragment(R.layout.fragment_university) {
+
+    lateinit var autoCompleteTextView: AutoCompleteTextView
+    lateinit var continueButton : Button
+    var selected = ""
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val view : View = inflater.inflate(R.layout.fragment_university, container, false)
+        autoCompleteTextView = view.findViewById(R.id.options)
+        continueButton = view.findViewById(R.id.continueButton)
+
+        val items = resources.getStringArray(R.array.universities)
+        val adapter = ArrayAdapter(requireContext(), R.layout.list_item, items)
+        autoCompleteTextView.setAdapter(adapter)
+        continueButton.setVisibility(View.INVISIBLE)
+        autoCompleteTextView.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                selected = s.toString()
+                continueButton.setVisibility(View.VISIBLE)
+            }
+
+            override fun beforeTextChanged(
+                s: CharSequence?,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+        })
+        // Inflate the layout for this fragment
+        return view
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+    }
+
+    fun onContinue(view: View)
+    {
+        //proceed to next screen with the selected university.
+    }
+}
