6e20106a15d863f015b49f38f62b890d89e111d1
Yao Zhang <alanzhangyao@outlook.com>
2022-03-16 16:52:50 -0400

    Not working

--- app/src/main/java/com/example/google_solution_challenge/QuestionFragment.kt
+++ app/src/main/java/com/example/google_solution_challenge/QuestionFragment.kt
@@ -11,15 +11,16 @@ import android.widget.TextView
 
 
 class QuestionFragment : Fragment() {
-    var page = 1;
+    var page = 1
+    var answerArray = arrayOf(-1, -1, -1, -1)
     lateinit var answer1 : Button
     lateinit var answer2 : Button
     lateinit var answer3 : Button
     lateinit var answer4 : Button
-    lateinit var question1 : TextView
-    lateinit var question2 : TextView
-    lateinit var question3 : TextView
-    lateinit var question4 : TextView
+    lateinit var question1 : Button
+    lateinit var question2 : Button
+    lateinit var question3 : Button
+    lateinit var question4 : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -39,24 +40,49 @@ class QuestionFragment : Fragment() {
         question2 = view.findViewById(R.id.question2)
         question3 = view.findViewById(R.id.question3)
         question4 = view.findViewById(R.id.question4)
-        question1.setTextColor(Color.WHITE)
+        question1.setTextColor(Color.BLACK)
         answer1.setOnClickListener(object: View.OnClickListener{
             override fun onClick(p0: View?) {
+                when (page){
+                    1-> answerArray[0] = 0
+                    2->answerArray[1] = 0
+                    3->answerArray[2] = 0
+                    4->answerArray[3] = 0
+                }
                 onAnswer()
             }
         })
         answer2.setOnClickListener(object: View.OnClickListener{
             override fun onClick(p0: View?) {
+
+                when (page){
+                    1-> answerArray[0] = 1
+                    2->answerArray[1] = 1
+                    3->answerArray[2] = 1
+                    4->answerArray[3] = 1
+                }
                 onAnswer()
             }
         })
         answer3.setOnClickListener(object: View.OnClickListener{
             override fun onClick(p0: View?) {
+                when (page){
+                    1-> answerArray[0] = 2
+                    2->answerArray[1] = 2
+                    3->answerArray[2] = 2
+                    4->answerArray[3] = 2
+                }
                 onAnswer()
             }
         })
         answer4.setOnClickListener(object: View.OnClickListener{
             override fun onClick(p0: View?) {
+                when (page){
+                    1-> answerArray[0] = 3
+                    2->answerArray[1] = 3
+                    3->answerArray[2] = 3
+                    4->answerArray[3] = 3
+                }
                 onAnswer()
             }
         })
@@ -67,22 +93,73 @@ class QuestionFragment : Fragment() {
     {
         if(page == 1){
             page = 2
-            question1.setTextColor(Color.BLACK)
-            question2.setTextColor(Color.WHITE)
+            question1.setTextColor(Color.WHITE)
+            question2.setTextColor(Color.BLACK)
 
         }
         else if (page == 2)
         {
             page = 3
-            question2.setTextColor(Color.BLACK)
-            question3.setTextColor(Color.WHITE)
+            question2.setTextColor(Color.WHITE)
+            question3.setTextColor(Color.BLACK)
         }
         else if (page == 3)
         {
             page = 4
-            question3.setTextColor(Color.BLACK)
-            question4.setTextColor(Color.WHITE)
+            question3.setTextColor(Color.WHITE)
+            question4.setTextColor(Color.BLACK)
         }
+    }
+    fun changeQuestion(view: View){
+        if(view.id == R.id.answer1)
+        {
+            if( page != 1){
+                page = 1
+                if(answerArray[0] != -1)
+                {
+                    when(answerArray[0]){
+                        0->answer1.setBackgroundColor(Color.BLUE)
+                        1->answer2.setBackgroundColor(Color.BLUE)
+                        2->answer3.setBackgroundColor(Color.BLUE)
+                        3->answer4.setBackgroundColor(Color.BLUE)
+                    }
+                }
+            }
+        }
+        else if(view.id == R.id.answer2)
+        {
+            if(page >= 2)
+            {
+                page = 2
+                if(answerArray[1] != -1)
+                {
+                    when(answerArray[1]){
+                        0->answer1.setBackgroundColor(Color.BLUE)
+                        1->answer2.setBackgroundColor(Color.BLUE)
+                        2->answer3.setBackgroundColor(Color.BLUE)
+                        3->answer4.setBackgroundColor(Color.BLUE)
+                    }
+                }
+            }
+        }
+        else if(view.id == R.id.answer3)
+        {
+            if(page >= 3)
+            {
+                page = 3
+                if(answerArray[2] != -1)
+                {
+                    when(answerArray[2]){
+                        0->answer1.setBackgroundColor(Color.BLUE)
+                        1->answer2.setBackgroundColor(Color.BLUE)
+                        2->answer3.setBackgroundColor(Color.BLUE)
+                        3->answer4.setBackgroundColor(Color.BLUE)
+                    }
+                }
+            }
+        }
+
+
     }
 
 }
