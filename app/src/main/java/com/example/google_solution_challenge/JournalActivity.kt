39c5570ae6ceab8bb637f25a33a5f85381e1c010
Miles123K <yh643@georgetown.edu>
2022-03-25 14:28:17 -0400

    pulling questions from database.

--- app/src/main/java/com/example/google_solution_challenge/JournalActivity.kt
+++ app/src/main/java/com/example/google_solution_challenge/JournalActivity.kt
@@ -1,50 +1,61 @@
 package com.example.google_solution_challenge
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.SharedPreferences
+import android.os.Build
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.CalendarView
 import android.widget.ListView
 import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import com.example.hw5.Answer
 import java.time.LocalDate
 
 class JournalActivity : AppCompatActivity() {
-    lateinit var list : ListView
+    lateinit var list: ListView
+
+    @RequiresApi(Build.VERSION_CODES.O)
     var selectedDate = LocalDate.now().toString()
-    lateinit var text : TextView
-    lateinit var calendar : CalendarView
+    lateinit var text: TextView
+    private lateinit var calendar: CalendarView
+
+    lateinit var sharedPreferences: SharedPreferences
+    var answerList = ArrayList<Answer>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_journal)
         text = findViewById(R.id.journal)
-        text.setText("Journal Log\n$selectedDate")
+        text.text = "Journal Log\n$selectedDate"
         calendar = findViewById(R.id.calendar)
         calendar.setOnDateChangeListener { view, year, month, dayOfMonth ->
             var monthString = (month + 1).toString()
             var dayOfMonthString = dayOfMonth.toString()
             if (month < 10) {
-                monthString = "0" + monthString
+                monthString = "0$monthString"
             }
             if (dayOfMonth < 10) {
-                dayOfMonthString = "0" + dayOfMonthString
+                dayOfMonthString = "0$dayOfMonthString"
             }
-            selectedDate = year.toString() + "-" + monthString + "-" + dayOfMonthString
-            text.setText("Journal Log\n$selectedDate")
+            selectedDate = "$year-$monthString-$dayOfMonthString"
+            text.text = "Journal Log\n$selectedDate"
             updateList()
 
         }
         list = findViewById(R.id.list)
-        answerList = ObjectSerializer.deserialize(sharedPreferences
-            .getString("answers", ObjectSerializer.serialize(ArrayList<Answer>()))) as ArrayList<Answer>
+        answerList = ObjectSerializer.deserialize(
+            sharedPreferences
+                .getString("answers", ObjectSerializer.serialize(ArrayList<Answer>()))
+        ) as ArrayList<Answer>
         updateList()
 
     }
-    fun updateList(){
+
+    private fun updateList() {
         var sublist = ArrayList<Answer>()
-        for (a in answerList){
-            if(a.date.equals(selectedDate))
-            {
+        for (a in answerList) {
+            if (a.date == selectedDate) {
                 sublist.add(a)
             }
         }
--- app/src/main/java/com/example/google_solution_challenge/ResourceActivity.kt
+++ app/src/main/java/com/example/google_solution_challenge/ResourceActivity.kt
@@ -1,18 +1,24 @@
 package com.example.google_solution_challenge
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.example.hw5.Answer
 
 class ResourceActivity : AppCompatActivity() {
+
+    lateinit var sharedPreferences: SharedPreferences
+    var answerList = ArrayList<Answer>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resource)
     }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val menuInflater: MenuInflater = menuInflater
         menuInflater.inflate(R.menu.bottom_nav_menu, menu)
@@ -22,7 +28,6 @@ class ResourceActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.journalButton -> {
-                intent = getIntent()
                 intent.setClass(this, JournalActivity::class.java)
                 startActivity(intent)
             }
