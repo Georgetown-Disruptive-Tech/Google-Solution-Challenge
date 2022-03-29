e0d970f42bbaf99acc4e0fe64aca586fc75b40e1
Yao Zhang <alanzhangyao@outlook.com>
2022-03-28 22:58:40 -0400

    Initial stab at recommendation algo

--- app/src/main/java/com/example/google_solution_challenge/ResourceActivity.kt
+++ app/src/main/java/com/example/google_solution_challenge/ResourceActivity.kt
@@ -14,6 +14,10 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.hw5.Answer
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 class ResourceActivity : AppCompatActivity() {
@@ -33,6 +37,7 @@ class ResourceActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resource)
         setTitle("Resources")
+
         generalResourceButton = findViewById(R.id.generalResources)
         nationalResourceButton = findViewById(R.id.nationalResources)
         stateResourceButton = findViewById(R.id.stateResources)
@@ -40,6 +45,63 @@ class ResourceActivity : AppCompatActivity() {
 
         sharedPreferences = getSharedPreferences("userInfo", Context.MODE_PRIVATE)
         data = getSharedPreferences("com.example.google_solution_challenge", Context.MODE_PRIVATE)
+        var answerList = ArrayList<Answer>()
+        answerList = ObjectSerializer.deserialize(
+            data
+                .getString("answers", ObjectSerializer.serialize(ArrayList<Answer>()))
+        ) as ArrayList<Answer>
+        val sublist = ArrayList<Answer>()
+        val flag = arrayOf(0, 0, 0)
+        val today = LocalDate.now().toString()
+        System.out.println(today)
+        val calendar: Calendar = Calendar.getInstance()
+        val format = SimpleDateFormat("yyyy-MM-dd")
+        val date : Date? = format.parse(today)
+
+        calendar.setTime(date)
+        calendar.add(Calendar.DATE, -1)
+        val yesterday: String = format.format(calendar.getTime())
+        calendar.add(Calendar.DATE, -1)
+        val twoDaysAgo :String = format.format(calendar.getTime())
+        //System.out.println(yesterdayAsString)
+        //System.out.println(twoDaysAgo)
+        for (a in answerList) {
+            if (a.date == twoDaysAgo) {
+                sublist.add(a)
+                flag[0] = 1
+            }
+            else if (a.date == yesterday){
+                sublist.add(a)
+                flag[1] = 1
+            }
+            else if (a.date == today){
+                sublist.add(a)
+                flag[2] = 1
+            }
+        }
+        if(flag[0] == 1 && flag[1] == 1 && flag[2] == 1)
+        {
+            //if user logs continuously for three days
+            //check signs of depression
+            var depressionMeter = 0
+            for(a in sublist){
+                if(a.question.contains("stress") && a.answer == "Yes") depressionMeter ++
+                if(a.question.contains("satisfied") && (a.answer == "Not" || a.answer == "Barely")) depressionMeter ++
+                if(a.question.contains("insomnia") && (a.answer.contains("insomnia") || a.answer == "asleep")) depressionMeter ++
+                if(a.question.contains("performance") && (a.answer == "Always")) depressionMeter ++
+                if(a.question.contains("guilt") && (a.answer == "Always")) depressionMeter ++
+                if(a.question.contains("purging") && (a.answer == "Yes")) depressionMeter ++
+                if(a.question.contains("social") && (a.answer == "Always")) depressionMeter ++
+                if(a.question.contains("intake") && (a.answer == "Yes")) depressionMeter ++
+            }
+            if (depressionMeter >= 4)
+            {
+                recommendedResourceURL = "https://www.everydayhealth.com/depression/guide/resources/"
+                recommendedResourceButton.setText("Recommended Resource - Depression")
+            }
+        }
+
+
         val db = Firebase.firestore
         val university = sharedPreferences.getString("University", "")
         val generalResources = db.collection("general-resources")
@@ -115,6 +177,14 @@ class ResourceActivity : AppCompatActivity() {
                 val uri: Uri = Uri.parse(generalResourceURL) // missing 'http://' will cause crashed
                 val intent = Intent(Intent.ACTION_VIEW, uri)
                 startActivity(intent)
+            }
+            R.id.recommendedResources -> {
+                if(recommendedResourceURL != "") {
+                    var uri: Uri =
+                        Uri.parse(recommendedResourceURL) // missing 'http://' will cause crashed
+                    val intent = Intent(Intent.ACTION_VIEW, uri)
+                    startActivity(intent)
+                }
             }
 
 
